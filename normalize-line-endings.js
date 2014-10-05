32cf6442db270eae9b119d1df31c42a0fb93301f
wooorm <tituswormer@gmail.com>
2014-10-05 09:30:54 +0200

    Fix CRLF line-endings from creeping into npm

---
+++ normalize-line-endings.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var fs,
+    eol;
+
+fs = require('fs');
+eol = require('eol');
+
+fs.readFile('data/udhr-txt/index.xml', 'utf-8', function (err, contents) {
+    if (err) {
+        throw err;
+    }
+
+    contents = eol.lf(contents);
+
+    fs.writeFile('data/udhr-txt/index.xml', contents, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+});
